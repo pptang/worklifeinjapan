4441e3395e0279b151793a1269c897f9dc494935
pptang <ptang8@bus.illinois.edu>
2017-04-10 07:47:45 +0900

    * add embedded query param to get media item within one request * modified php files to strip out html tag of content and excerpt

--- pages/ArticleListPage.js
+++ pages/ArticleListPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Body, Title, Content, Left, Right, Icon, Text, Button, Card, CardItem } from 'native-base';
+import { Container, Header, Body, Title, Content, Left, Right, Icon, Text, Button, Card, CardItem, Thumbnail } from 'native-base';
 import { ARTICLE_LIST } from '../utils/constants';
 
 export default class ArticleListPage extends Component {
@@ -10,15 +10,15 @@ export default class ArticleListPage extends Component {
       posts: []
     };
   }
-  componentWillMount() {
+  componentDidMount() {
     this.fetchArticles();
   }
   fetchArticles() {
-    fetch(ARTICLE_LIST)
+    return fetch(ARTICLE_LIST)
       .then(res => res.json())
       .then((res) => {
         this.setState({
-          posts: res.posts
+          posts: res
         });
       })
       .catch(err => console.error(err));
@@ -40,12 +40,21 @@ export default class ArticleListPage extends Component {
         <Content>
           {
             this.state.posts.map((post) => {
+              console.error(post);
               return (
-                <Card key={post.id}>
+                <Card key={post.id} style={{ flex: 0 }}>
                   <CardItem>
-                    <Body>
-                      <Text>{post.title}</Text>
-                    </Body>
+                    <Left>
+                      <Body>
+                        <Text>{post.title.rendered}</Text>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem cardBody>
+                    <Thumbnail style={{ resizeMode: 'cover' }} square source={{ uri: post._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url }} />
+                  </CardItem>
+                  <CardItem content>
+                    <Text>{post.my_excerpt}</Text>
                   </CardItem>
                 </Card>
               );
--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,1 @@
-export const ARTICLE_LIST = 'http://35.187.203.223/?json=1';
+***REMOVED***

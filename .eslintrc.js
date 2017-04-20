b9ef4f9749018399da859492f2c509964b46c2fb
Tang <paipo.tang@p42223.intra.rakuten.co.jp>
2017-04-20 20:39:05 +0900

    * add firebase push notification setup for android * add eslint * add pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-quotes": 0,
+        "react/jsx-filename-extension": 0,
+        "global-require": 1,
+        "max-len": 0
+    }
+};

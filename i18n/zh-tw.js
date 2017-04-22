6ab5bbabb95d8c0a591c9687642571a0461d0466
pptang <ptang8@bus.illinois.edu>
2017-04-22 09:37:46 +0900

    * modify linting

--- i18n/zh-tw.js
+++ i18n/zh-tw.js
@@ -1,13 +1,15 @@
-export default lang = {
-  "new_article_list": "最新文章",
-  "business_venture_article_list": "商業趨勢", // 169
-  "find_your_way_article_list": "日本求職", // 3
-  "english_article_list": "英文文章", // 193
-  "japanese_article_list": "日文文章", // 171
-  "japan_insight_article_list": "日本洞見", // 4
-  "japan_life_article_list": "日本生活", // 173
-  "share_story_article_list": "人物專訪", // 6
-  "wij_member_article_list": "WIJ成員分享", // 172
-  "uncategorized_article_list": "未分類", // 1
-  "continue_reading": "繼續閱讀",
+const lang = {
+  new_article_list: '最新文章',
+  business_venture_article_list: '商業趨勢', // 169
+  find_your_way_article_list: '日本求職', // 3
+  english_article_list: '英文文章', // 193
+  japanese_article_list: '日文文章', // 171
+  japan_insight_article_list: '日本洞見', // 4
+  japan_life_article_list: '日本生活', // 173
+  share_story_article_list: '人物專訪', // 6
+  wij_member_article_list: 'WIJ成員分享', // 172
+  uncategorized_article_list: '未分類', // 1
+  continue_reading: '繼續閱讀',
 };
+
+export default lang;
--- pages/ArticlesPage.js
+++ pages/ArticlesPage.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Container, Header, Body, Title, Content, Left, Right, Icon, Text, Button, Card, CardItem, Spinner, H3, Thumbnail } from 'native-base';
-import { Image, View } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { Container, Body, Content, Left, Right, Text, Button, Card, CardItem, Spinner, H3, Thumbnail } from 'native-base';
+import { View } from 'react-native';
 import Moment from 'moment';
 import NavBar from '../components/NavBar';
 import { ARTICLE_LIST } from '../utils/constants';
@@ -12,7 +12,7 @@ export default class ArticlesPage extends Component {
     this.fetchArticles = this.fetchArticles.bind(this);
     this.state = {
       categoryId: null,
-      posts: []
+      posts: [],
     };
   }
   componentDidMount() {
@@ -25,37 +25,34 @@ export default class ArticlesPage extends Component {
   // }
   fetchArticles(categoryId) {
     if (this.totalPages && this.page < this.totalPages) {
-      this.page++;
+      this.page = this.page + 1;
     } else if (this.page === this.totalPage) {
-      alert('no more article');
-      return
+      // alert('no more article');
+      return;
     }
     const url = categoryId ?
       `${ARTICLE_LIST}&categories=${categoryId}&page=${this.page}`
       :
       `${ARTICLE_LIST}&page=${this.page}`;
-    return fetch(url)
-      .then(res => {
+    fetch(url)
+      .then((res) => {
         this.totalPages = res.headers.map['x-wp-totalpages'];
         return res.json();
       })
       .then((res) => {
         this.isLoading = false;
-        this.setState((prevState) => {
-          return {
-            posts: [...prevState.posts, ...res]
-          }
-        });
+        this.setState(prevState => ({
+          posts: [...prevState.posts, ...res],
+        }));
       })
       .catch(err => console.error(err));
   }
 
   render() {
-
     return (
       <Container>
         <NavBar navigation={this.props.navigation} title={this.props.navigation.state.params.title} goBack={false} />
-        {/*<Header style={{ backgroundColor: '#fff' }}>
+        {/* <Header style={{ backgroundColor: '#fff' }}>
           <Left>
             <Button transparent onPress={() => this.props.navigation.navigate('DrawerOpen')}>
               <Icon name="menu" style={{color: '#b51d22'}} />
@@ -77,31 +74,30 @@ export default class ArticlesPage extends Component {
         >
           {
             this.state.posts.length > 0 ?
-              this.state.posts.map((post) => {
-                return (
-                  <Card key={post.id} style={{ flex: 0 }}>
-                    <CardItem header>
-                      <Left>
-                        <Body>
-                          <Text note>{`${Moment(post.date).format('YYYY-MM-DD')} / By ${post._embedded.author[0].name}`}</Text>
-                        </Body>
-                      </Left>
-                    </CardItem>
-                    <CardItem>
-                      <Left>
-                        <Body>
-                          <H3>{post.title.rendered}</H3>
-                        </Body>
-                      </Left>
-                      <Thumbnail
-                        style={{ marginLeft: 10 }}
-                        square
-                        source={{
-                          uri: post._embedded['wp:featuredmedia'][0].media_details.sizes['thumbnail'].source_url
-                        }}
-                      />
-                    </CardItem>
-                    {/*<CardItem cardBody>
+              this.state.posts.map(post => (
+                <Card key={post.id} style={{ flex: 0 }}>
+                  <CardItem header>
+                    <Left>
+                      <Body>
+                        <Text note>{`${Moment(post.date).format('YYYY-MM-DD')} / By ${post._embedded.author[0].name}`}</Text>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem>
+                    <Left>
+                      <Body>
+                        <H3>{post.title.rendered}</H3>
+                      </Body>
+                    </Left>
+                    <Thumbnail
+                      style={{ marginLeft: 10 }}
+                      square
+                      source={{
+                        uri: post._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url,
+                      }}
+                    />
+                  </CardItem>
+                  {/* <CardItem cardBody>
                       <Image
                         style={{ width: 400, height: 200 }}
                         source={{
@@ -110,41 +106,40 @@ export default class ArticlesPage extends Component {
                             : post._embedded['wp:featuredmedia'][0].media_details.sizes['portfolio-default'].source_url
                         }} />
                     </CardItem>*/}
-                    <CardItem content>
-                      <Left>
-                        <Body>
-                          <Text>
-                            {
+                  <CardItem content>
+                    <Left>
+                      <Body>
+                        <Text>
+                          {
                               post.my_excerpt
                                 .replace(/&nbsp;/g, '')
                                 .substr(0, 100)
                             }......
                       </Text>
-                        </Body>
-                      </Left>
-                    </CardItem>
-                    <CardItem footer>
-                      <Right>
-                        <Button danger bordered rounded onPress={() => this.props.navigation.navigate('DetailPage', { post, title: this.props.navigation.state.params.title })}>
-                          <Text>{lang.continue_reading}</Text>
-                        </Button>
-                      </Right>
-                    </CardItem>
-                  </Card>
-                );
-              })
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem footer>
+                    <Right>
+                      <Button danger bordered rounded onPress={() => this.props.navigation.navigate('DetailPage', { post, title: this.props.navigation.state.params.title })}>
+                        <Text>{lang.continue_reading}</Text>
+                      </Button>
+                    </Right>
+                  </CardItem>
+                </Card>
+                ))
               :
-              <Spinner color='red' />
+              <Spinner color="red" />
           }
           {
             this.state.posts.length > 0 ?
               <View
-                ref={ref => this.view = ref}
+                ref={(ref) => { this.view = ref; }}
                 onLayout={({ nativeEvent }) => {
                   this.spinnerOffsetTop = nativeEvent.layout.y;
                 }}
               >
-                <Spinner color='red' />
+                <Spinner color="red" />
               </View>
               :
               null
@@ -155,3 +150,17 @@ export default class ArticlesPage extends Component {
     );
   }
 }
+
+ArticlesPage.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func,
+    navigate: PropTypes.func,
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        title: PropTypes.string,
+        categoryId: PropTypes.string,
+      }),
+    }).isRequired,
+  }).isRequired,
+  title: PropTypes.string.isRequired,
+};

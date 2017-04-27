31d4a1d023be86b33de97f6c6f6c906951b8fcd1
pptang <ptang8@bus.illinois.edu>
2017-04-27 21:36:34 +0900

    *Add youtube video page

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,3 @@
 ***REMOVED***
 ***REMOVED***
+export const CHANNEL_VIDEO_LIST = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDm4AryQYxPWWI0VnsufJFUlELIvZ59I7E&channelId=UCM0SQUVSu-bnspF1CZ4qZbw&part=snippet,id&order=date&maxResults=20';

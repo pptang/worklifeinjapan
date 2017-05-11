954b1f47ac1c0454735eba1035fce3496a1b0eda
pptang <ptang8@bus.illinois.edu>
2017-05-11 22:04:29 +0900

    *. Add social share functionality

--- utils/constants.js
+++ utils/constants.js
@@ -2,3 +2,6 @@ export const ARTICLE_LIST = 'http://www.worklifeinjapan.net/wp-json/wp/v2/posts?
 export const CATEGORY_LIST = 'http://www.worklifeinjapan.net/wp-json/wp/v2/categories';
 export const CHANNEL_VIDEO_LIST = 'https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNEL_ID}&part=snippet,id&order=date&maxResults=20';
 export const ERROR_MESSAGE = '網路連線異常';
+export const SHARE_CANCELL = '已取消分享';
+export const SHARE_SUCCESS = '分享成功！';
+export const SHARE_FAIL = '分享失敗！';

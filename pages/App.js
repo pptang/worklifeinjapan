429d5dbba1470d92318352fbc7329b8e88e70728
pptang <donaldduck518@gmail.com>
2017-05-24 21:27:08 +0900

    Add status bar icon and direct click action to detail page

--- pages/App.js
+++ pages/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import MainPage from './MainPage';
 import ErrorBar from '../components/ErrorBar';
 import { CATEGORY_LIST } from '../utils/constants';
-import FCM, { FCMEvent } from 'react-native-fcm';
 
 export default class App extends Component {
   constructor(props) {
@@ -11,10 +10,8 @@ export default class App extends Component {
       categories: [],
       isShowingError: false,
     };
-    this.handlePushNotification.bind(this);
   }
   componentDidMount() {
-    this.handlePushNotification();
     fetch(CATEGORY_LIST)
       .then(res => res.json())
       .then((res) => {
@@ -23,62 +20,6 @@ export default class App extends Component {
         });
       })
       .catch(() => this.setState({ isShowingError: true }));
-  }
-
-  componentWillUnmount() {
-    // stop listening for events
-    this.notificationListener.remove();
-    this.refreshTokenListener.remove();
-  }
-
-  handlePushNotification() {
-    FCM.requestPermissions(); // for iOS
-    FCM.getFCMToken().then((token) => {
-      console.log('token:', token);
-      // store fcm token in your server
-    });
-    FCM.subscribeToTopic('new-article');
-    this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
-      console.log('notif::', notif);
-      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-      if (notif.local_notification) {
-        // this is a local notification
-        console.log('local_notification::', notif);
-      }
-      if (notif.opened_from_tray) {
-        console.log('notif_opened_tray::', notif);
-        // app is open/resumed because user clicked banner
-      }
-      // await someAsyncCall();
-
-      // if (Platform.OS === 'ios') {
-      //   //optional
-      //   //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-      //   //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-      //   //notif._notificationType is available for iOS platfrom
-      //   switch (notif._notificationType) {
-      //     case NotificationType.Remote:
-      //       notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-      //       break;
-      //     case NotificationType.NotificationResponse:
-      //       notif.finish();
-      //       break;
-      //     case NotificationType.WillPresent:
-      //       notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
-      //       break;
-      //   }
-      // }
-
-      FCM.presentLocalNotification({                                     // Android only, LED blinking (default false)
-        large_icon: 'ic_launcher',                           // Android only
-        icon: 'ic_launcher',
-        show_in_foreground,                                // notification when app is in foreground (local & remote)
-      });
-    });
-    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, (token) => {
-      console.log('refresh::', token);
-      // fcm token may not be available on first load, catch it here
-    });
   }
 
   render() {

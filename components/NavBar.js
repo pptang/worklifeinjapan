f6467822ac58ba042aa4b79a52d39212105b31c0
pptang <donaldduck518@gmail.com>
2017-05-24 21:27:08 +0900

    Add status bar icon and direct click action to detail page

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { Header, Body, Title, Left, Right, Icon, Button } from 'native-base';
 import { Share, TextInput } from 'react-native';
-import { SHARE_CANCELL, SHARE_SUCCESS, SHARE_FAIL } from '../utils/constants';
-
+import { SHARE_CANCELL, SHARE_SUCCESS, SHARE_FAIL, GET_POST } from '../utils/constants';
+import FCM, { FCMEvent } from 'react-native-fcm';
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -13,7 +13,19 @@ export default class Navbar extends Component {
       isSearching: false,
       searchText: '',
       result: '',
+      isShowingError: false,
     };
+    this.handlePushNotification.bind(this);
+  }
+
+  componentDidMount() {
+    this.handlePushNotification();
+  }
+
+  componentWillUnmount() {
+    // stop listening for events
+    this.notificationListener.remove();
+    this.refreshTokenListener.remove();
   }
 
   _shareText() {
@@ -42,6 +54,57 @@ export default class Navbar extends Component {
       alert(SHARE_CANCELL);
       this.setState({ result: 'dismissed' });
     }
+  }
+
+  handlePushNotification() {
+    FCM.requestPermissions(); // for iOS
+    FCM.getFCMToken().then((token) => {
+      console.log('token:', token);
+      // store fcm token in your server
+    });
+    FCM.subscribeToTopic('new-article');
+    FCM.subscribeToTopic('new-video');
+    this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
+      // console.log('notif::', notif);
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+      // if (notif.local_notification) {
+      //   // this is a local notification
+      //   console.log('local_notification::', notif);
+      // }
+      if (notif.opened_from_tray) {
+        // app is open/resumed because user clicked banner
+        const url = `${GET_POST}${notif.article_id}?_embed=true`;
+        fetch(url)
+          .then(res => res.json())
+          .then((res) => {
+            this.props.navigation.navigate('DetailPage', { post: res });
+          })
+          .catch(() => this.setState({ isShowingError: true }));
+      }
+      // await someAsyncCall();
+
+      // if (Platform.OS === 'ios') {
+      //   //optional
+      //   //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
+      //   //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
+      //   //notif._notificationType is available for iOS platfrom
+      //   switch (notif._notificationType) {
+      //     case NotificationType.Remote:
+      //       notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
+      //       break;
+      //     case NotificationType.NotificationResponse:
+      //       notif.finish();
+      //       break;
+      //     case NotificationType.WillPresent:
+      //       notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
+      //       break;
+      //   }
+      // }
+    });
+    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, (token) => {
+      console.log('refresh::', token);
+      // fcm token may not be available on first load, catch it here
+    });
   }
 
   render() {

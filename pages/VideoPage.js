12d5337a3469dfb45551fb2a870d4297dd8cd283
pptang <donaldduck518@gmail.com>
2017-05-31 07:00:08 +0900

    Add api key to youtube component and add release configuration

--- pages/VideoPage.js
+++ pages/VideoPage.js
@@ -4,7 +4,7 @@ import { Container, Content, Card, CardItem, Body, Left, H3, Text } from 'native
 import Moment from 'moment';
 import NavBar from '../components/NavBar';
 import ErrorBar from '../components/ErrorBar';
-import { CHANNEL_VIDEO_LIST } from '../utils/constants';
+import { CHANNEL_VIDEO_LIST, YOUTUBE_API_KEY } from '../utils/constants';
 
 export default class VideoPage extends Component {
   constructor(props) {
@@ -65,13 +65,14 @@ export default class VideoPage extends Component {
                   <CardItem content>
                     <Body>
                       <YouTube
+                        apiKey={YOUTUBE_API_KEY}
                         videoId={video.id.videoId}
                         play={this.state.isPlaying}
                         hidden={false}
                         onReady={() => { this.setState({ isReady: true }); }}
                         onChangeState={(e) => { this.setState({ status: e.state }); }}
                         onChangeQuality={(e) => { this.setState({ quality: e.quality }); }}
-                        onError={(e) => { this.setState({ error: e.error }); }}
+                        onError={() => { this.setState({ isShowingError: true }); }}
                         style={{ alignSelf: 'stretch', height: 200, backgroundColor: 'black', marginVertical: 20, marginHorizontal: 5 }}
                       />
                     </Body>
--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,8 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-export const CHANNEL_VIDEO_LIST = 'https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId=UCM0SQUVSu-bnspF1CZ4qZbw&part=snippet,id&order=date&maxResults=20';
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***

22972ebd2df5a3ace55808834bcb770ad7fa66bc
pptang <donaldduck518@gmail.com>
2017-06-09 21:48:23 +0900

    remove key

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,9 @@
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+export const ARTICLE_LIST = 'http://www.worklifeinjapan.net/wp-json/wp/v2/posts?_embed=true';
+export const GET_POST = 'http://www.worklifeinjapan.net/wp-json/wp/v2/posts/';
+export const CATEGORY_LIST = 'http://www.worklifeinjapan.net/wp-json/wp/v2/categories';
+export const YOUTUBE_API_KEY = '';
+export const CHANNEL_VIDEO_LIST = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=UCM0SQUVSu-bnspF1CZ4qZbw&part=snippet,id&order=date&maxResults=20`;
+export const ERROR_MESSAGE = '網路連線異常';
+export const SHARE_CANCELL = '已取消分享';
+export const SHARE_SUCCESS = '分享成功！';
+export const SHARE_FAIL = '分享失敗！';
